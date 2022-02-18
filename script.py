21030afeb66d1995dbe7e362640ee1d42a6962c0
OyugoObonyo <nyikal1999@gmail.com>
2022-02-18 19:26:51 +0300

    Fix shutil error

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-from os import path
 
 extensions = {
     "COMPRESSED": ["tar", "zip", "rar"],
@@ -10,6 +9,11 @@ extensions = {
     "VIDEOS": ["mp4", "mkv", "mov", "wmv", "webm", "avi", "avchd", "3gp", "mpeg", "mpg"]
 }
 
+# Create directories with key names
+for key in extensions:
+    if not os.path.exists(key):
+        os.mkdir(key)
+
 def check_is_file(file):
     """
     Checks if an input is a file or not
@@ -18,8 +22,7 @@ def check_is_file(file):
         return True
     return False
 
-current_dir = os.getcwd()
-
+# Check all files in the directory
 for file_name in os.listdir('.'):
     checker = check_is_file(file_name)
     if checker == False:
@@ -28,4 +31,4 @@ for file_name in os.listdir('.'):
         extension = file_name.split('.')[-1]
         for key,value in extensions.items():
             if extension in extensions[key]:
-                shutil.move(os.path.join(current_dir, file_name), key)
+                shutil.move(file_name, key)
